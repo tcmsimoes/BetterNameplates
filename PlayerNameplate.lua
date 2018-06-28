cfc155e6bfde01a784117260a1f1da51aefed08f
Banksy <445668+tcmsimoes@users.noreply.github.com>
2018-06-28 09:06:09 +0200

    Avoided taint on setting player nameplate color

--- PlayerNameplate.lua
+++ PlayerNameplate.lua
@@ -1,10 +1,20 @@
 local function SetupPlayerNamePlate(frame, setupOptions, frameOptions)
-    if frameOptions.displayName == false then -- UnitIsUnit(frame.displayedUnit, "player") doesnt work?!
+    if not frameOptions.displayName then
         frame.healthBar:SetHeight(12)
-
-        frame.optionTable.healthBarColorOverride = nil
-        frame.optionTable.useClassColors = true
+        frame.healthBar:SetStatusBarTexture("Interface\\AddOns\\BetterNameplates\\media\\bar_serenity")
     end
 end
 
 hooksecurefunc("DefaultCompactNamePlateFrameSetupInternal", SetupPlayerNamePlate)
+
+local function UpdatePlayerNamePlateHealthColor(frame)
+    if not frame.optionTable.displayName then
+        local localizedClass, englishClass = UnitClass(frame.unit);
+        local classColor = RAID_CLASS_COLORS[englishClass];
+        r, g, b = classColor.r, classColor.g, classColor.b;
+        frame.healthBar:SetStatusBarColor(r, g, b);
+        frame.healthBar.r, frame.healthBar.g, frame.healthBar.b = r, g, b;
+    end
+end
+
+hooksecurefunc("CompactUnitFrame_UpdateHealthColor", UpdatePlayerNamePlateHealthColor)
