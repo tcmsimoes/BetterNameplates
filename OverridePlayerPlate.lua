5b5c1dc72ad3ca85bad72e62bbda1a8569d6a47b
Banksy <445668+tcmsimoes@users.noreply.github.com>
2020-11-03 23:20:51 +0100

    fixed player nameplate issues

--- OverridePlayerPlate.lua
+++ OverridePlayerPlate.lua
@@ -9,18 +9,20 @@ hooksecurefunc("CompactUnitFrame_UpdateHealthColor", function(frame)
     if UnitIsPlayer(frame.unit) then
         local localizedClass, englishClass = UnitClass(frame.unit);
         local classColor = RAID_CLASS_COLORS[englishClass];
-        r, g, b = classColor.r, classColor.g, classColor.b;
+        if classColor then
+            local r, g, b = classColor.r, classColor.g, classColor.b;
 
-        if ( r ~= frame.healthBar.r or g ~= frame.healthBar.g or b ~= frame.healthBar.b ) then
-            frame.healthBar:SetStatusBarColor(r, g, b);
-    
-            if (frame.optionTable.colorHealthWithExtendedColors) then
-                frame.selectionHighlight:SetVertexColor(r, g, b);
-            else
-                frame.selectionHighlight:SetVertexColor(1, 1, 1);
+            if r ~= frame.healthBar.r or g ~= frame.healthBar.g or b ~= frame.healthBar.b then
+                frame.healthBar:SetStatusBarColor(r, g, b);
+        
+                if frame.optionTable.colorHealthWithExtendedColors then
+                    frame.selectionHighlight:SetVertexColor(r, g, b);
+                else
+                    frame.selectionHighlight:SetVertexColor(1, 1, 1);
+                end
+        
+                frame.healthBar.r, frame.healthBar.g, frame.healthBar.b = r, g, b;
             end
-    
-            frame.healthBar.r, frame.healthBar.g, frame.healthBar.b = r, g, b;
         end
     end
 end);
